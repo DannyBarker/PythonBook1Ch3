18d173bba2091cb0fa5668208d3d1a1f77050b98
Danny Barker <barkerdanny17@yahoo.com>
2019-08-26 13:14:54 -0500

    finished randomNumbers exc

---
+++ randomNumbers.py
@@ -0,0 +1,13 @@
+import random
+
+my_randoms = list()
+for i in range(10):
+    my_randoms.append(random.randrange(1, 6, 1))
+print(my_randoms)
+numb_list = list(range(1, 11))
+for number in numb_list:
+    x = False
+    for list_Num in my_randoms:
+        if list_Num == number:
+            x = True
+    print(f"My random list {'contains' if x else 'does not contain'} {number}")
